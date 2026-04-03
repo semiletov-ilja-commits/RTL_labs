1ef140b052e17d3b8ad02a75136148f0b0c89c67
semiletov-ilja-commits <semiletov-ilja@yandex.ru>
2026-04-03 22:59:12 +0300

    Add files via upload
    Lab 1. Cnt_bcd

---
+++ clk_div.sv
@@ -0,0 +1,23 @@
+module clk_diverse (
+    input logic clk,
+    input logic rst,
+    output logic clk_div
+);
+    parameter max_count = 25_000_000;
+    
+    logic [24:0] counter;
+    
+    always_ff @(posedge clk or negedge rst) begin
+        if (!rst) begin  
+            counter <= 0;
+				clk_div <= 0;
+        end else begin
+            if (counter == max_count - 1) begin
+                counter <= 0;
+					 clk_div <= ~clk_div;
+            end else
+                counter <= counter + 1;
+        end
+    end
+    
+endmodule
